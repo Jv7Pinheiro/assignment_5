5ac52cc837ef288acae41500dd0bc2e37ce1c0e0
Alejandro Gonzalez Rubio Abdala <gonza224@gemini.deac.wfu.edu>
2025-04-02 14:27:51 -0400

    draft for part3

---
+++ scripts/assignment5_part3_recommendation.py
@@ -0,0 +1,134 @@
+import sys, os, warnings
+
+import gzip
+import time
+import numpy as np
+import pandas as pd
+
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import StringIndexer # type: ignore
+from pyspark.ml.recommendation import ALS # type: ignore
+from pyspark.ml.evaluation import RegressionEvaluator # type: ignore
+from pyspark.sql.functions import col, log, pow, greatest, lit, isnan # type: ignore
+
+import utils
+warnings.filterwarnings("ignore")
+os.environ["PYSPARK_SUBMIT_ARGS"] = "--driver-memory 128g pyspark-shell"
+
+LOG_TRANSFORM = True
+CLEANED_DATA_PATH = '../data/steam_reviews_clean_data.json'
+
+def main():
+    
+    if not os.path.exists(CLEANED_DATA_PATH):
+        # Load File
+        input_file = gzip.open("/deac/csc/classes/csc373/data/assignment_5/steam_reviews.json.gz")
+
+        # Append Data
+        appending_time_start = time.time()
+        dataset = []
+        for i, l in enumerate(input_file):
+            # if i > 1000:
+            #     break
+            d = eval(l)
+            dataset.append(d)
+        input_file.close()
+        appending_time_end = time.time()
+        print(f"Loading and Appending Data time: {appending_time_end - appending_time_start:.6f} seconds\n")
+
+
+        # Clean Data
+        cleaning_time_start = time.time()
+        data = utils.clean_data(dataset, required_features=["hours", "early_access", "text"])
+        cleaning_time_end = time.time()
+        print(f"Cleaning time: {cleaning_time_end - cleaning_time_start:.6f} seconds\n")
+        
+        cleaned_df = pd.DataFrame(data)
+        cleaned_df.to_json(CLEANED_DATA_PATH, orient="records", lines=True)
+        print(f"Cleaned data saved to {CLEANED_DATA_PATH}\n")
+
+    spark_df = spark.read.json(CLEANED_DATA_PATH)
+    print("Cleaned data loaded into Spark DataFrame.\n")
+
+    train_spark_df, dev_spark_df = spark_df.randomSplit([0.8, 0.2], seed=42)
+    print(f"Train count: {train_spark_df.count()}, Dev count: {dev_spark_df.count()}\n")
+
+    ####################
+    ## Recommendation ##
+    ####################
+
+    spark = (
+        SparkSession.builder
+            .master("local")
+            .appName("Hours Recommendation")
+            .getOrCreate()
+    )
+
+    user_stringIndexer = StringIndexer(inputCol="username", outputCol="user_index", handleInvalid="keep")
+    product_stringIndexer = StringIndexer(inputCol="product_id", outputCol="product_index", handleInvalid="keep")
+
+    user_model = user_stringIndexer.fit(train_spark_df)
+    product_model = product_stringIndexer.fit(train_spark_df)
+
+    train_spark_df = user_model.transform(train_spark_df)
+    train_spark_df = product_model.transform(train_spark_df)
+
+    dev_spark_df = user_model.transform(dev_spark_df)
+    dev_spark_df = product_model.transform(dev_spark_df)
+    
+    if LOG_TRANSFORM:
+        train_spark_df = train_spark_df.withColumn("hours", log(2.0, col("hours") + 1))
+
+    # Create the ALS model
+    als = ALS(
+        maxIter=5,
+        regParam=0.1,
+        rank=10,
+        seed=0,
+        userCol="user_index",
+        itemCol="product_index",
+        ratingCol="hours",
+    )
+
+    # Recommender training
+    REC_train_time_start = time.time()
+    als_model = als.fit(train_spark_df)
+    REC_train_time_end = time.time()
+    print(f"\tRecommender training time: {REC_train_time_end - REC_train_time_start:.6f} seconds")
+
+    # Predict Dev hours using regressor
+    REC_predict_time_start = time.time()
+    predictions = als_model.transform(dev_spark_df)
+    REC_predict_time_end = time.time()
+    print(f"\tRecommender predict time: {REC_predict_time_end - REC_predict_time_start:.6f} seconds\n")
+
+    if LOG_TRANSFORM:
+        predictions = predictions.withColumn("prediction", pow(2.0, greatest(col("prediction"), lit(0))) - 1)
+
+    predictions = predictions.filter(
+        col("prediction").isNotNull() & ~isnan("prediction") &
+        col("hours").isNotNull() & ~isnan("hours")
+    )
+
+    # Gather and print information about regressor's estimations
+    evaluator = RegressionEvaluator(
+        labelCol="hours",
+        predictionCol="prediction",
+        metricName="rmse"
+    )
+
+    total = predictions.count()
+    dev_rmse = evaluator.evaluate(predictions) 
+    underpred = predictions.filter(predictions.prediction < predictions.hours).count()
+    overpred = predictions.filter(predictions.prediction > predictions.hours).count()
+
+    print(f"\tRegressor MSE on dev dataset: {dev_rmse:.2f}")
+    print(f"\tUnderpredictions: {underpred}")
+    print(f"\tUnderprediction rate: {(underpred / total)*100}%")
+    print(f"\tOverpredictions:  {overpred}")
+    print(f"\tOverprediction rate rate: {(overpred / total)*100}%")
+
+
+
+if __name__ == "__main__":
+    main()
